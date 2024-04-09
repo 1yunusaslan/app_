7823d35ff947692be6d4dd94cafed09d190fbadb
Yunus Emre ASLAN <unuvitec@gmail.com>
2024-04-09 17:07:43 +0300

    düzenleme

--- assets/js/main.js
+++ assets/js/main.js
@@ -159,7 +159,26 @@ nav.className= 'link-container';
 navcontainer.className = 'nav-container';
 container.className = 'container';
 
-data.eng.navbar.a.forEach(link =>{
+const content = document.getElementById('content');
+const content_maker = document.getElementById('content-maker');
+const content_img =  document.getElementById('content-img');
+const content_main= document.getElementById('content-main');
+
+content.className = 'content col-12 col-sm-12';
+content_maker.className= 'row';
+content_img.className = 'content-img col-12 col-md-6 ';
+content_main.className= 'content-main col-12 col-md-6';
+
+          
+const footer = document.getElementById('footer');
+const footer_container = document.getElementById('footer-container');
+const footer_link_container = document.getElementById('footer-link-container');
+
+footer.className='footer';
+footer_container.className='footer-container';
+footer_link_container.className='link-container';
+
+data.tr.navbar.a.forEach(link =>{
      const a = document.createElement('a');
      a.textContent = link.name;
      a.href= link.href;
@@ -173,7 +192,7 @@ data.eng.navbar.a.forEach(link =>{
 })
 
 const select = document.createElement('select');
-data.eng.navbar.selection.forEach(link =>{
+data.tr.navbar.selection.forEach(link =>{
      const option = document.createElement('option');
      option.textContent=link.name;
      option.value= link.value;
@@ -186,17 +205,8 @@ data.eng.navbar.selection.forEach(link =>{
 })
 
 
-const content = document.getElementById('content');
-const content_maker = document.getElementById('content-maker');
-const content_img =  document.getElementById('content-img');
-const content_main= document.getElementById('content-main');
 
-content.className = 'content col-12 col-sm-12';
-content_maker.className= 'row';
-content_img.className = 'content-img col-12 col-md-6 ';
-content_main.className= 'content-main col-12 col-md-6';
-
-data.eng.content.img.forEach(link =>{
+data.tr.content.img.forEach(link =>{
      const img = document.createElement('img');
      img.src = link.src;
 
@@ -206,7 +216,7 @@ data.eng.content.img.forEach(link =>{
      container.appendChild(content);
 })
 
-data.eng.content.main.forEach(link =>{
+data.tr.content.main.forEach(link =>{
      const h1 = document.createElement('h1');
      h1.textContent=link.tittle;
 
@@ -227,15 +237,9 @@ data.eng.content.main.forEach(link =>{
 
 
 
-const footer = document.getElementById('footer');
-const footer_container = document.getElementById('footer-container');
-const footer_link_container = document.getElementById('footer-link-container');
 
-footer.className='footer';
-footer_container.className='footer-container';
-footer_link_container.className='link-container';
 
-data.eng.footer.outside_a.forEach(link =>{
+data.tr.footer.outside_a.forEach(link =>{
      const a = document.createElement('a');
      a.textContent = link.name;
      a.href = link.href;
@@ -247,7 +251,7 @@ data.eng.footer.outside_a.forEach(link =>{
 })
 
 
-data.eng.footer.inside_a.forEach(link =>{
+data.tr.footer.inside_a.forEach(link =>{
      const a = document.createElement('a');
      a.textContent = link.name;
      a.href = link.href;
@@ -260,22 +264,203 @@ data.eng.footer.inside_a.forEach(link =>{
 })
 
 
+
+
+
+
 document.getElementById('sec').addEventListener('change', function() {
      var secilen_dil = this.value;
-   
 
      changeLanguage(secilen_dil);
 })
 
 
 function changeLanguage(language) {
+
     
      if (language === 'english') {
 
+          data.eng.navbar.a.forEach(link =>{
+               const a = document.createElement('a');
+               a.textContent = link.name;
+               a.href= link.href;
+               a.className = 'link';
+          
+               nav.appendChild(a);
+               navcontainer.appendChild(nav);
+               container.prepend(navcontainer);
+          
+          })
+          
+          const select = document.createElement('select');
+          data.eng.navbar.selection.forEach(link =>{
+               const option = document.createElement('option');
+               option.textContent=link.name;
+               option.value= link.value;
+          
+               select.appendChild(option);
+               nav.appendChild(select);
+               navcontainer.appendChild(nav);
+               container.prepend(navcontainer);
+          
+          })
+          
+          
+
+          
+          data.eng.content.img.forEach(link =>{
+               const img = document.createElement('img');
+               img.src = link.src;
+          
+               content_img.appendChild(img);
+               content_maker.appendChild(content_img);
+               content.appendChild(content_maker);
+               container.appendChild(content);
+          })
+          
+          data.eng.content.main.forEach(link =>{
+               const h1 = document.createElement('h1');
+               h1.textContent=link.tittle;
+          
+               const h3 = document.createElement('h3');
+               h3.textContent=link.subtittle;
+          
+               const p = document.createElement('p');
+               p.textContent= link.about;
+          
+               content_main.appendChild(h1);
+               content_main.appendChild(h3);
+               content_main.appendChild(p);
+          
+               content_maker.appendChild(content_main);
+               content.appendChild(content_maker);
+               container.appendChild(content);
+
+          })
+          
+          
+
+          
+          data.eng.footer.outside_a.forEach(link =>{
+               const a = document.createElement('a');
+               a.textContent = link.name;
+               a.href = link.href;
+               a.className= link.class;
+          
+               footer_container.appendChild(a);
+               footer.appendChild(footer_container);
+               container.appendChild(footer);
+          })
+          
+          
+          data.eng.footer.inside_a.forEach(link =>{
+               const a = document.createElement('a');
+               a.textContent = link.name;
+               a.href = link.href;
+               a.className= link.class;
+          
+               footer_link_container.appendChild(a);
+               footer_container.appendChild(footer_link_container);
+               footer.appendChild(footer_container);
+               container.appendChild(footer);
+          })
+
+
      } 
      
-     else if (language ==='turkce') {
+     else if (language === 'turkce') {
+          
 
+
+          data.tr.navbar.a.forEach(link =>{
+               const a = document.createElement('a');
+               a.textContent = link.name;
+               a.href= link.href;
+               a.className = 'link';
+          
+               nav.appendChild(a);
+               navcontainer.appendChild(nav);
+               container.prepend(navcontainer);
+
+               
+          
+          })
+          
+          const select = document.createElement('select');
+          data.tr.navbar.selection.forEach(link =>{
+               const option = document.createElement('option');
+               option.textContent=link.name;
+               option.value= link.value;
+          
+               select.appendChild(option);
+               nav.appendChild(select);
+               navcontainer.appendChild(nav);
+               container.prepend(navcontainer);
+          
+          })
+          
+
+          
+          data.tr.content.img.forEach(link =>{
+               const img = document.createElement('img');
+               img.src = link.src;
+          
+               content_img.appendChild(img);
+               content_maker.appendChild(content_img);
+               content.appendChild(content_maker);
+               container.appendChild(content);
+          })
+          
+          data.tr.content.main.forEach(link =>{
+               const h1 = document.createElement('h1');
+               h1.textContent=link.tittle;
+          
+               const h3 = document.createElement('h3');
+               h3.textContent=link.subtittle;
+          
+               const p = document.createElement('p');
+               p.textContent= link.about;
+          
+               content_main.appendChild(h1);
+               content_main.appendChild(h3);
+               content_main.appendChild(p);
+          
+               content_maker.appendChild(content_main);
+               content.appendChild(content_maker);
+               container.appendChild(content);
+          })
+          
+          
+          
+
+          
+          data.tr.footer.outside_a.forEach(link =>{
+               const a = document.createElement('a');
+               a.textContent = link.name;
+               a.href = link.href;
+               a.className= link.class;
+          
+               footer_container.appendChild(a);
+               footer.appendChild(footer_container);
+               container.appendChild(footer);
+          })
+          
+          
+          data.tr.footer.inside_a.forEach(link =>{
+               const a = document.createElement('a');
+               a.textContent = link.name;
+               a.href = link.href;
+               a.className= link.class;
+          
+               footer_link_container.appendChild(a);
+               footer_container.appendChild(footer_link_container);
+               footer.appendChild(footer_container);
+               container.appendChild(footer);
+          })
+
+
+          
+      
      }
 
 }
